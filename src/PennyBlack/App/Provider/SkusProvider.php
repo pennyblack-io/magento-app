e89b8e28d5391bea3d436a5bc157543a8f504156
Jack Barton <jack@pennyblack.io>
2023-02-28 08:34:46 +0000

    MISC Add type hints to provider arrow funcs

--- src/PennyBlack/App/Provider/SkusProvider.php
+++ src/PennyBlack/App/Provider/SkusProvider.php
@@ -2,12 +2,13 @@
 
 namespace PennyBlack\App\Provider;
 
+use Magento\Sales\Api\Data\OrderItemInterface;
 use Magento\Sales\Model\Order;
 
 class SkusProvider
 {
     public function get(Order $order): array
     {
-        return array_map(fn($item): string => $item->getSku(), $order->getItems());
+        return array_map(fn(OrderItemInterface $item): string => $item->getSku(), $order->getItems());
     }
 }
